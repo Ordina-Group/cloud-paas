404e6f5d89b478d7f28b3c99e347293631337b91
samet10 <samet1gundogan@gmail.com>
2016-01-28 15:24:00 +0100

    opmaak is aangepast chat is toegevoegd naam invullen toegevoegd

--- workshops/cf-intro/index.js
+++ workshops/cf-intro/index.js
@@ -1,11 +1,25 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-app.get('/', function (request, response) {
-    var naam = request.query.naam;
-    response.send('<h1>Hello ' + naam + '</h1>');
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+    socket.on('chat message', function(msg){
+        console.log('message: ' + msg);
+    });
 });
 
 http.listen(3000, function () {
     console.log('listening on *:3000');
 });
+
+
+
+
+
+
+
