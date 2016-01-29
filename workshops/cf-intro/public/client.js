7624e3cb73bbb9d96239dad43beb580938009df4
samet10 <samet1gundogan@gmail.com>
2016-01-29 12:46:35 +0100

    uitleg toegevoegd

--- workshops/cf-intro/public/client.js
+++ workshops/cf-intro/public/client.js
@@ -1,9 +1,9 @@
 var socket = io();
-$('#chatFormulier').submit(function () {
-    socket.emit('chat', $('#chatBericht').val());
+$('#chatFormulier').submit(function () {                //als submit geactiveerd word dan wordt tussen {} uitgevoerd
+    socket.emit('chat', $('#chatBericht').val());       //stuurt berichten naar alle sockets
     $('#chatBericht').val('');
     return false;
 });
-socket.on('chat', function (chatBericht) {
-    $('#messages').append($('<li>').text(chatBericht));
+socket.on('chat', function (chatBericht) {                  //chatbericht wordt geactiveerd, --> {}
+    $('#messages').append($('<li>').text(chatBericht));     //berichten zullen onder elkaar geplaatst worden
 });
--- workshops/cf-intro/server.js
+++ workshops/cf-intro/server.js
@@ -1,27 +1,27 @@
-var express = require('express');
-var app = express();
+var express = require('express');                       //je wilt express gebruiken
+var app = express();                                    // je sla express in variable app op
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.use(express.static('public'));
+app.use(express.static('public'));                      //verwijzen waar bestanden staan
 
-var sockets = [];
+var sockets = [];                                       //hier staan de gegevens van een gebruiker op als ze net verbonden zijn
 
-io.on('connection', function (socket) {
-    console.log('New connection: ' + socket);
-    sockets.push(socket);
+io.on('connection', function (socket) {                 //dat activeert wanneer klant verbonden wordt. io wilt verbinden zeggen
+    console.log('New connection: ' + socket);           //socket is browser
+    sockets.push(socket);                               //server stuurt bericht naar de klant
     socket.on('disconnect', function () {
-        console.log('Socket ' + socket + ' disconnected!');
+        console.log('Socket ' + socket + ' disconnected!');                 //klant verlaat
         //sockets(socket);
     });
-    socket.on('chat', function (chatBericht) {
-        console.log('Chat from socket ' + socket + ': ' + chatBericht);
+    socket.on('chat', function (chatBericht) {                           //als function(chatBericht) wordt geactiveerd, dan wordt tussen de {} uitgevoerd
+        console.log('Chat from socket ' + socket + ': ' + chatBericht);     //in command line verschijn "chat from socket .naam. : .bericht."
         for (var eenSocket in sockets) {
-            eenSocket.emit('chat', chatBericht);
+            eenSocket.emit('chat', chatBericht);                            //een variable eensocket stuurt berichten naar verschillende klanten
         }
     });
 });
 
-http.listen(3000, function () {
-    console.log('listening on *:3000');
+http.listen(3000, function () {                                             //je kiest de poort 3000
+    console.log('listening on *:3000');                                     //in command line verschijn string 'listening on *:3000'
 });
