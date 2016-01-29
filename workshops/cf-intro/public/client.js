c3495364b88cb46ed0c242881049e6ceda0d5c0b
Dieter Hubau <dhubau@gmail.com>
2016-01-29 14:08:23 +0100

    updated THE GAME

--- workshops/cf-intro/public/client.js
+++ workshops/cf-intro/public/client.js
@@ -1,9 +1,21 @@
 var socket = io();
-$('#chatFormulier').submit(function () {                //als submit geactiveerd word dan wordt tussen {} uitgevoerd
-    socket.emit('chat', $('#chatBericht').val());       //stuurt berichten naar alle sockets
+$('#chatFormulier').submit(function () {                // als submit geactiveerd word dan wordt tussen {} uitgevoerd
+    socket.emit('chat', $('#chatBericht').val());       // stuurt berichten naar de server
     $('#chatBericht').val('');
     return false;
 });
-socket.on('chat', function (chatBericht) {                  //chatbericht wordt geactiveerd, --> {}
-    $('#messages').append($('<li>').text(chatBericht));     //berichten zullen onder elkaar geplaatst worden
+$('#playFormulier').submit(function () {                // als submit geactiveerd word dan wordt tussen {} uitgevoerd
+    socket.emit('play', $('#naam').val());              // stuurt berichten naar de server
+    $('#naam').hide();
+    return false;
+});
+socket.on('chat', function (data) {                  // we krijgen een chatbericht van de server --> {}
+    $('#messages').append($('<li>').text(data));     // berichten zullen onder elkaar geplaatst worden
+});
+socket.on('play', function (data) {                  // we krijgen een chatbericht van de server --> {}
+    console.log('Player ' + data.player + ' [' + data.name + '] wil spelen! HOERA!')
+    if (data.player == 2) {
+        console.log('LET THE GAMES BEGIN !!!');
+        $('#playButton').hide();
+    }
 });
--- workshops/cf-intro/server.js
+++ workshops/cf-intro/server.js
@@ -1,27 +1,55 @@
-var express = require('express');                       //je wilt express gebruiken
-var app = express();                                    // je sla express in variable app op
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var expressAPI = require('express'); // Express API inladen
+var httpAPI = require('http'); // HTTP API inladen
+var socketioAPI = require('socket.io'); // socket IO API inladen
 
-app.use(express.static('public'));                      //verwijzen waar bestanden staan
+var app = expressAPI();  // je slaat een nieuwe express app in variable app op
+var httpServer = httpAPI.Server(app); // creeer een nieuwe HTTP server voor de express app
+var io = socketioAPI(httpServer); // creeer een nieuwe socket IO object voor deze HTTP server
 
-var sockets = [];                                       //hier staan de gegevens van een gebruiker op als ze net verbonden zijn
+app.use(expressAPI.static('public'));                      // verwijzen waar publieke bestanden staan - zijn voor iedereen toegankelijk
 
-io.on('connection', function (socket) {                 //dat activeert wanneer klant verbonden wordt. io wilt verbinden zeggen
-    console.log('New connection: ' + socket);           //socket is browser
-    sockets.push(socket);                               //server stuurt bericht naar de klant
+var sockets = [];                                       // hier staat de client informatie in (IP, poort, socket, etc)
+var players = [];
+
+io.on('connection', function (socket) {                 // dat activeert wanneer klant verbonden wordt. io wilt input/output zeggen
+    console.log('New connection: ' + socket);           // socket is een browser connectie met de server
+    sockets.push(socket);                               // in de array van sockets voegen we de nieuwe socket toe
     socket.on('disconnect', function () {
-        console.log('Socket ' + socket + ' disconnected!');                 //klant verlaat
-        //sockets(socket);
+        console.log('Socket ' + socket + ' disconnected!');                 // klant sluit de browser of gaat naar een andere website
     });
-    socket.on('chat', function (chatBericht) {                           //als function(chatBericht) wordt geactiveerd, dan wordt tussen de {} uitgevoerd
-        console.log('Chat from socket ' + socket + ': ' + chatBericht);     //in command line verschijn "chat from socket .naam. : .bericht."
-        for (var eenSocket in sockets) {
-            eenSocket.emit('chat', chatBericht);                            //een variable eensocket stuurt berichten naar verschillende klanten
+    socket.on('play', function (name) { // een speler duwt op 'PLAY'
+        console.log('Player ' + name + ' wants to play!');
+        if (players.length < 2) { // er mogen maximum 2 spelers zijn
+            players.push(name); // voeg de speler zijn naam toe aan de lijst van spelers
+            for (var i = 0; i < sockets.length; i++) { // stuur naar alle connecties een bericht dat een speler is toegevoegd
+                if (players.length == 1) { // speler 1 is net toegevoegd
+                    sockets[i].emit('play', {
+                        name: name,
+                        player: 1
+                    });
+                } else { // speler 2 is net toegevoegd
+                    sockets[i].emit('play', {
+                        name: name,
+                        player: 2
+                    });
+                }
+            }
+        } else {
+            console.log('Player ' + name + ' cannot play because there are already 2 players!');
         }
+    });
+    socket.on('chat', function (chatBericht) {                           // als we van de socket een 'chat' bericht krijgen, dan wordt de onderstaande functie uitgevoerd
+        console.log('Chat from socket ' + socket + ': ' + chatBericht);     //in command line verschijn "chat from socket .naam. : .bericht."
+        for (var i = 0; i < sockets.length; i++) {
+            sockets[i].emit('chat', chatBericht);                            //een variable eensocket stuurt berichten naar verschillende klanten
+        }
+    });
+    socket.on('gamedata', function (data) {                           // als we van de socket een 'chat' bericht krijgen, dan wordt de onderstaande functie uitgevoerd
+        console.log('Game data ontvangen: ' + data);     //in command line verschijn "chat from socket .naam. : .bericht."
+
     });
 });
 
-http.listen(3000, function () {                                             //je kiest de poort 3000
+httpServer.listen(3000, function () {                                             //je kiest de poort 3000
     console.log('listening on *:3000');                                     //in command line verschijn string 'listening on *:3000'
 });
