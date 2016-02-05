8f7e26bc5bcbd171f22912dba89e56cc70fcfb90
samet10 <samet1gundogan@gmail.com>
2016-02-05 10:45:20 +0100

    update veranderen allemaal

--- workshops/cf-intro/server.js
+++ workshops/cf-intro/server.js
@@ -9,40 +9,40 @@ var io = socketioAPI(httpServer); // creeer een nieuwe socket IO object voor dez
 app.use(expressAPI.static('public'));                      // verwijzen waar publieke bestanden staan - zijn voor iedereen toegankelijk
 
 var sockets = [];                                       // hier staat de client informatie in (IP, poort, socket, etc)
-var gamedata = {
+var gamedata = {                                        // gamedata is een dataobject ---> hij heeft 3 objecten namelijk players, keuzes, winner
     players: [],
     keuzes: [],
     winner: undefined
 };
-var chatMessages = [];
+var chatMessages = [];                                  // hier worden berichten opgeslagen
 
 io.on('connection', function (socket) {                 // dat activeert wanneer klant verbonden wordt. io wilt input/output zeggen
     console.log('New connection: ' + socket);           // socket is een browser connectie met de server
-    sockets.push(socket);                               // in de array van sockets voegen we de nieuwe socket toe
+    sockets.push(socket);                               // in de array van sockets voegen we de nieuwe socket toe. push wilt zetten zeggen
     socket.on('disconnect', function () {
         console.log('Socket ' + socket + ' disconnected!');                 // klant sluit de browser of gaat naar een andere website
     });
-    socket.on('play', function (name) { // een speler duwt op 'PLAY'
+    socket.on('play', function (name) {             // socket.on --> server wacht tot speler op een 'PLAY' drukt
         console.log('Player ' + name + ' wants to play!');
-        if (gamedata.players.length < 2) { // er mogen maximum 2 spelers zijn
-            gamedata.players.push(name); // voeg de speler zijn naam toe aan de lijst van spelers
-            for (var i = 0; i < sockets.length; i++) { // stuur naar alle connecties een bericht dat een speler is toegevoegd
-                if (gamedata.players.length == 1) { // speler 1 is net toegevoegd
-                    sockets[i].emit('play', {
-                        name: name,
+        if (gamedata.players.length < 2) {              // er mogen maximum 2 spelers zijn
+            gamedata.players.push(name);                // voeg de speler zijn naam toe aan de lijst van spelers
+            for (var i = 0; i < sockets.length; i++) {  // stuur naar alle connecties een bericht dat een speler is toegevoegd
+                if (gamedata.players.length == 1) {     // speler 1 is net toegevoegd
+                    sockets[i].emit('play', {           // als speler 1 er is, stuur naar alle connecties --> naam, number
+                        name: name,                     // je hebt je naam ingetypt in client.js
                         number: 1,
-                        self: sockets[i] == socket
+                        self: sockets[i] == socket      // je kan zelf ook je name en number zien
                     });
-                } else { // speler 2 is net toegevoegd
-                    sockets[i].emit('play', {
+                } else {                                // speler 2 is net toegevoegd
+                    sockets[i].emit('play', {           // laat name, number van speler 2 zien aan alle connecties
                         name: name,
                         number: 2,
-                        self: sockets[i] == socket
+                        self: sockets[i] == socket      // speler 2 kan zelf zijn name en number zien
                     });
                 }
             }
         } else {
-            console.log('Player ' + name + ' cannot play because there are already 2 players!');
+            console.log('Player ' + name + ' cannot play because there are already 2 players!'); // als er in de array players al 2 namen heeft. dan staat die text in de console
         }
     });
     socket.on('chat', function (chatBericht) {                           // als we van de socket een 'chat' bericht krijgen, dan wordt de onderstaande functie uitgevoerd
@@ -54,11 +54,11 @@ io.on('connection', function (socket) {                 // dat activeert wanneer
     });
     socket.on('gamedata', function (data) {                           // als we van de socket een 'gamedata' bericht krijgen, dan wordt de onderstaande functie uitgevoerd
         console.log('Game data ontvangen van speler ' + data.player.number + ': ' + data.keuze);     // in command line verschijn "chat from socket .naam. : .bericht."
-        gamedata.keuzes[data.player.number - 1] = data.keuze;  // wordt opgeslagen in gamedata -> welke speler keuze heeft gemaakt(schaar, steen,...) en -1 -> omdat array met 0 begint
-        if (gamedata.keuzes[0] != undefined && gamedata.keuzes[1] != undefined) {
+        gamedata.keuzes[data.player.number - 1] = data.keuze; // wordt opgeslagen in gamedata -> welke speler keuze heeft gemaakt(schaar, steen,...) en -1 -> omdat array met 0 begint
+        if (gamedata.keuzes[0] != undefined && gamedata.keuzes[1] != undefined) {           // keuzes mogen niet leeg zijn
             gamedata.winner = determineWinner(gamedata.keuzes[0], gamedata.keuzes[1]) ? 0 : 1;
             emitToAll('gamedata', gamedata);
-            console.log('Winner: ' + (gamedata.winner ? 'Speler 1' : 'Speler 2'));
+            console.log('Winner: ' + (gamedata.winner ? 'Speler 1' : 'Speler 2'));      // vraagteken wilt zeggen wie wint het
         }
     });
 });
@@ -68,15 +68,15 @@ httpServer.listen(3000, function () {                                           
 });
 
 
-function emitToAll(messageType, message) {
+function emitToAll(messageType, message) {            //je kan nu altijd emitToAll gebruiken in plaats van sockets.emit() --> gemakkelijker
     for (var i = 0; i < sockets.length; i++) {
         sockets[i].emit(messageType, message);
     }
 }
 
 function determineWinner(choice1, choice2) {
-    if (choice1 == choice2) {
-        return undefined;                                   //gelijk
+    if (choice1 == choice2) {                               //gelijk
+        return undefined;
     }
     switch (choice1) {
         case 1:
@@ -102,15 +102,5 @@ function determineWinner(choice1, choice2) {
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
 
 
