2587c6f9db76b2cf171588b07bc6c946908bf52a
Tim Ysewyn <Tim.Ysewyn@ordina.be>
2016-02-15 17:47:35 +0100

    Added CORS to allow everyone - DO NOT USE IN PRODUCTION

--- workshops/game-data-api/src/main/java/be/ordina/jworks/GameDataApiApplication.java
+++ workshops/game-data-api/src/main/java/be/ordina/jworks/GameDataApiApplication.java
@@ -2,7 +2,11 @@ package be.ordina.jworks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @ComponentScan
 @SpringBootApplication
@@ -10,5 +14,15 @@ public class GameDataApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GameDataApiApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/*").allowedOrigins("*");
+            }
+        };
     }
 }
